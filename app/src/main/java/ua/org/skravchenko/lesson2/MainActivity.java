2e6803fc4478228887928258f97a8daeaed969e9
Serhii <srh.kravchenko@gmail.com>
2018-03-06 21:04:48 +0200

    Login page

--- app/src/main/java/ua/org/skravchenko/lesson2/MainActivity.java
+++ app/src/main/java/ua/org/skravchenko/lesson2/MainActivity.java
@@ -1,129 +1,41 @@
 package ua.org.skravchenko.lesson2;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-  public static final int SECOND_ACTIVITY_REQUEST_CODE = 1;
-  public static final String R_COLOR_STRING = "R_COLOR_STRING";
-  public static final String G_COLOR_STRING = "G_COLOR_STRING";
-  public static final String B_COLOR_STRING = "B_COLOR_STRING";
+  EditText etLoginName;
+  EditText etPassword;
+  Button bLogin;
 
-  private int rColor = 255;
-  private int gColor = 255;
-  private int bColor = 255;
-  private EditText etRColor;
-  private EditText etGColor;
-  private EditText etBColor;
-  private View vColorScreen;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    vColorScreen = findViewById(R.id.vColorScreen);
-    etRColor = findViewById(R.id.etRColor);
-    etGColor = findViewById(R.id.etGColor);
-    etBColor = findViewById(R.id.etBColor);
+    bLogin = (Button)findViewById(R.id.bLogin);
+    etPassword = (EditText)findViewById(R.id.etPassword);
 
-    etRColor.setOnKeyListener(new View.OnKeyListener() {
-      @Override public boolean onKey(View view, int i, KeyEvent keyEvent) {
-        if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && (i == KeyEvent.KEYCODE_ENTER)) {
-          rColor = Integer.parseInt(etRColor.getText().toString());
-          if (rColor > 255) {
-            etRColor.setText("255");
-            rColor = 255;
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          } else {
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          }
-
-          return true;
-        }
-
-        return false;
-      }
-    });
-
-    etGColor.setOnKeyListener(new View.OnKeyListener() {
-      @Override public boolean onKey(View view, int i, KeyEvent keyEvent) {
-        if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && (i == KeyEvent.KEYCODE_ENTER)) {
-          gColor = Integer.parseInt(etGColor.getText().toString());
-          if (gColor > 255) {
-            etGColor.setText("255");
-            gColor = 255;
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          } else {
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          }
-
-          return true;
-        }
-
-        return false;
-      }
-    });
-    etBColor.setOnKeyListener(new View.OnKeyListener() {
-      @Override public boolean onKey(View view, int i, KeyEvent keyEvent) {
-        if (keyEvent.getAction() == KeyEvent.ACTION_DOWN && (i == KeyEvent.KEYCODE_ENTER)) {
-          bColor = Integer.parseInt(etBColor.getText().toString());
-          if (bColor > 255) {
-            etBColor.setText("255");
-            bColor = 255;
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          } else {
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-          }
-
-          return true;
-        }
-
-        return false;
-      }
-    });
-    Button bNextActivity = findViewById(R.id.bNextActivity);
-
-    bNextActivity.setOnClickListener(new View.OnClickListener() {
+    bLogin.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-        int rColorForIntent = Integer.parseInt(etRColor.getText().toString());
-        int gColorForIntent = Integer.parseInt(etGColor.getText().toString());
-        int bColorForIntent = Integer.parseInt(etBColor.getText().toString());
-        intent.putExtra(R_COLOR_STRING, rColorForIntent);
-        intent.putExtra(G_COLOR_STRING, gColorForIntent);
-        intent.putExtra(B_COLOR_STRING, bColorForIntent);
-        startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
+        if(TextUtils.isEmpty(etPassword.getText())){
+          etPassword.setError("Enter password please!!!");
+        }else if (etPassword.getText().length()<6){
+          etPassword.setError("Enter please more than 6 digits!!!");
+        }else{
+          Intent intent = new Intent(getApplicationContext(),BookList.class);
+          startActivity(intent);
+        }
       }
     });
+
   }
 
-  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
 
-    switch (requestCode) {
-      case SECOND_ACTIVITY_REQUEST_CODE:
-        switch (resultCode) {
-          case Activity.RESULT_OK:
-            Bundle bundle = data.getExtras();
-            int rColor = bundle.getInt(R_COLOR_STRING, 255);
-            int gColor = bundle.getInt(G_COLOR_STRING, 255);
-            int bColor = bundle.getInt(B_COLOR_STRING, 255);
-            vColorScreen.setBackgroundColor(Color.rgb(rColor, gColor, bColor));
-            etRColor.setText(String.format("%d", rColor));
-            etGColor.setText(String.format("%d", gColor));
-            etBColor.setText(String.format("%d", bColor));
-            break;
-          case Activity.RESULT_CANCELED:
-            break;
-        }
-    }
-  }
 }
